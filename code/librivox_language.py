7706e68e0ea4991848c61aebd86103ae86edbcbf
Yorwba <yorwb4@gmail.com>
2020-10-19 17:25:55 +0200

    Get the language a book is written in

---
+++ code/librivox_language.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as f: info = json.load(f)
+
+    for book in info['books'].values():
+        print(book['language'])
