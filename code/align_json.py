97b11c4318afb56aea31f363e2cfb020a19183d9
Yorwba <yorwb4@gmail.com>
2020-11-10 21:24:41 +0100

    Factor out generally useful functions from SRT generation code

---
+++ code/align_json.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import json
+
+
+def span_text(alignment):
+    text = alignment['span']
+    if isinstance(text, str):
+        return text
+    else:
+        return text['text']
+
+
+def is_standout(paragraph):
+    return all(
+        line[-1].isalnum()
+        for line in span_text(paragraph).splitlines()
+    )
+
+
+def vad_pad(items, extend_before=1., extend_after=1.):
+    extension = extend_after + extend_before
+    items = iter(items)
+    current = next(items).copy()
+    current['start_time'] = max(0.0, current['start_time'] - extend_before)
+    for item in items:
+        item = item.copy()
+        end = item['start_time']
+        delta = end - current['end_time']
+        assert delta >= 0.0
+        if delta >= extension:
+            current['end_time'] += extend_after
+        else:
+            scale = delta / extension
+            current['end_time'] += scale * extend_after
+            item['start_time'] -= scale * extend_before
+        yield current
+        current = item
+    current['end_time'] += extend_after
+    yield current
--- code/srt.py
+++ code/srt.py
@@ -1,47 +1,15 @@
 #!/usr/bin/env python3
 
+import align_json
 import json
 import math
 import sys
 
 
-def span_text(alignment):
-    text = alignment['span']
-    if isinstance(text, str):
-        return text
-    else:
-        return text['text']
-
-
 def split_paragraph(paragraph):
-    if all(
-            line[-1].isalnum()
-            for line in span_text(paragraph).splitlines()
-    ):
+    if align_json.is_standout(paragraph):
         return [paragraph]
     return paragraph['subalignments']
-
-
-def vad_pad(items, extend_before=1., extend_after=1.):
-    extension = extend_after + extend_before
-    items = iter(items)
-    current = next(items).copy()
-    current['start_time'] = max(0.0, current['start_time'] - extend_before)
-    for item in items:
-        item = item.copy()
-        end = item['start_time']
-        delta = end - current['end_time']
-        assert delta >= 0.0
-        if delta >= extension:
-            current['end_time'] += extend_after
-        else:
-            scale = delta / extension
-            current['end_time'] += scale * extend_after
-            item['start_time'] -= scale * extend_before
-        yield current
-        current = item
-    current['end_time'] += extend_after
-    yield current
 
 
 def format_srt_time(time):
@@ -56,7 +24,7 @@ if __name__ == '__main__':
     with open(sys.argv[1]) as f: alignments = json.load(f)
 
     sub_index = 0
-    for split in vad_pad(
+    for split in align_json.vad_pad(
             split
             for paragraph in alignments
             for split in split_paragraph(paragraph)
@@ -64,5 +32,5 @@ if __name__ == '__main__':
         sub_index += 1
         print(sub_index)
         print('{} --> {}'.format(format_srt_time(split['start_time']), format_srt_time(split['end_time'])))
-        print(span_text(split).strip())
+        print(align_json.span_text(split).strip())
         print()
