f50581941ae7f47e02ca9257d12083ca6fc6d50e
Yorwba <yorwb4@gmail.com>
2020-12-06 20:25:36 +0100

    Allow getting info for individual sections

--- code/librivox_json.py
+++ code/librivox_json.py
@@ -9,13 +9,28 @@ def get_books(path):
     books = info['books']
     if hasattr(books, 'values'):
         books = list(books.values())
+    if not path.endswith('.json'):
+        filename = path.split('/')[-1].split('.')[0]
+        if filename:
+            sections = [
+                s
+                for b in books
+                for s in b.get('sections', [])
+                if filename in (s['file_name'] or '')
+            ]
+            if sections:
+                return sections
     return books
 
 
 def get_info(path):
     if os.path.isfile(path):
-        with open(path) as f:
-            return json.load(f)
+        if path.endswith('.json'):
+            with open(path) as f:
+                return json.load(f)
+        else:
+            up = os.path.dirname(path)
+            return get_info(up)
     if os.path.isdir(path):
         subpath = os.path.join(path, 'librivox.json')
         if os.path.isfile(subpath):
