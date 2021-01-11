ae1f157636c4ddaf0d2af0fd87ba44f5764a1ce8
Yorwba <yorwb4@gmail.com>
2021-01-11 20:25:29 +0100

    Use dynamic programming for better paragraph splitting

--- code/find_splits.py
+++ code/find_splits.py
@@ -7,11 +7,13 @@ import json
 import math
 import sys
 
+
 def num_lines(text, characters_per_line):
     return sum(
         max(1, int(math.ceil(len(line.rstrip())/characters_per_line)))
         for line in text.rstrip().split('\n')
     )
+
 
 def split_paragraph(paragraph, language=None):
     characters_per_line, lines_per_paragraph = defaultdict(
@@ -20,46 +22,67 @@ def split_paragraph(paragraph, language=None):
         jpn=(19, 5),
         yue=(19, 7),
     )[language]
+
     if num_lines(align_json.span_text(paragraph), characters_per_line) <= lines_per_paragraph:
         return [paragraph]
-    best_break = None
-    best_delta = -1
-    text_so_far = ''
-    previous_end = None
-    for i, sub in enumerate(paragraph['subalignments']):
-        text_so_far += align_json.span_text(sub)
-        lines_so_far = num_lines(text_so_far, characters_per_line)
-        if lines_so_far > lines_per_paragraph:
-            break
-        if any(c.isalnum() for c in align_json.span_text(sub)) or not previous_end:
-            previous_end = sub['end_time']
-        next_sub = paragraph['subalignments'][i+1]
-        delta = next_sub['start_time'] - previous_end
-        if delta >= best_delta:
-            best_delta = delta
-            best_break = i
-    if best_break is None:
-        split_first = split_paragraph(paragraph['subalignments'][0], language=language)
+
+    subs = paragraph['subalignments']
+
+    if len(subs) == 1:
+        split_single = split_paragraph(subs[0], language=language)
         new_paragraph = {
             'start_time': paragraph['start_time'],
             'end_time': paragraph['end_time'],
             'span': paragraph['span'],
-            'subalignments': split_first + paragraph['subalignments'][1:],
+            'subalignments': split_single,
         }
         return split_paragraph(new_paragraph, language=language)
-    paragraph_before_break = {
-        'start_time': paragraph['start_time'],
-        'end_time': paragraph['subalignments'][best_break]['end_time'],
-        'span': ''.join(align_json.span_text(sub) for sub in paragraph['subalignments'][:best_break+1]),
-        'subalignments': paragraph['subalignments'][:best_break+1],
-    }
-    paragraph_after_break = {
-        'start_time': paragraph['subalignments'][best_break+1]['start_time'],
-        'end_time': paragraph['end_time'],
-        'span': ''.join(align_json.span_text(sub) for sub in paragraph['subalignments'][best_break+1:]),
-        'subalignments': paragraph['subalignments'][best_break+1:],
-    }
-    return [paragraph_before_break] + split_paragraph(paragraph_after_break, language=language)
+
+    # dynamic programming to find the collection of breaks with minimal badness
+    break_before = [dict(predecessor=None, badness=0.)] # always break before 0
+    previous_end = None
+    for i, isub in enumerate(subs):
+        if any(c.isalnum() for c in align_json.span_text(isub)) or not previous_end:
+            previous_end = isub['end_time']
+
+        if i+1 == len(subs):
+            badness = 0.
+        else:
+            next_sub = subs[i+1]
+            delta = next_sub['start_time'] - previous_end
+            badness = 1./(delta + 0.001)
+
+        best_break = i
+        for j in range(i, -1, -1):
+            text_from_j_to_i = ''.join(align_json.span_text(jsub) for jsub in subs[j:i+1])
+            lines_from_j_to_i = num_lines(text_from_j_to_i, characters_per_line)
+            if lines_from_j_to_i > lines_per_paragraph:
+                break
+            if break_before[j]['badness'] < break_before[best_break]['badness']:
+                best_break = j
+
+        break_before.append(dict(
+            predecessor=best_break,
+            badness=badness + break_before[best_break]['badness'],
+        ))
+
+    breaks = [len(subs)] # always break at the end
+    while break_before[breaks[-1]]['predecessor'] is not None:
+        breaks.append(break_before[breaks[-1]]['predecessor'])
+    breaks = breaks[::-1]
+
+    broken = []
+    for start, end in zip(breaks, breaks[1:]):
+        paragraph_between_breaks = {
+            'start_time': (paragraph if start == 0 else subs[start])['start_time'],
+            'end_time': (paragraph if end == len(subs) else subs[end-1])['end_time'],
+            'span': ''.join(align_json.span_text(sub) for sub in subs[start:end]),
+            'subalignments': subs[start:end],
+        }
+        broken += split_paragraph(paragraph_between_breaks, language=language)
+
+    return broken
+
 
 def main(argv):
     parser = argparse.ArgumentParser(
@@ -83,6 +106,7 @@ def main(argv):
     if new_text != old_text:
         with open(args.output, 'w') as f:
             f.write(new_text)
+
 
 if __name__ == '__main__':
     main(sys.argv)
